395bfe5c4ff3b62d689cf29794b1e9a47afe627f
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2020-01-14 09:10:56 -0500

    added card stuff

--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import FilterList from "../components/FilterList";
 import useServicesList from "../hooks/useServicesList";
-import { Card, CardContent } from "@baltimorecounty/dotgov-components";
+import { Card, CardContent, CardFooter } from "@baltimorecounty/dotgov-components";
+
+const compare=(a,b)=>{
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+
+  let comparison = 0;
+  if (nameA > nameB) {
+    comparison = 1;
+  } else if (nameA < nameB) {
+    comparison = -1;
+  }
+  return comparison;
+};
+
 
 const ServiceList = () => {
-  const serviceItems = useServicesList();
-
+  const serviceItems = useServicesList().sort(compare);
+  let starImg = { fontSize:'.2em' , float:'right', marginTop: '0px', width: '10px'};
   const serviceCard = ({ name, department, url, icon, rank }) => (
-    <Card id={name}>
-      <CardContent>
-        <h3>{name}</h3>
-        <p>{department}</p>
-        <a href={url}>{name}</a>
-      </CardContent>
-    </Card>
+    <div className="d-flex col-lg-4 col-md-6 col-sm-6">
+      <Card>
+        <CardContent>
+          <div className="dg_icon-container">
+            <i className={icon} aria-hidden="true"></i>
+            <i className="rank" style={starImg} aria-hidden="true"/>
+          </div>
+          <h3>{name}</h3>
+        </CardContent>
+        <CardFooter>
+          <a href={url}> {department}</a>
+        </CardFooter>
+      </Card>
+   </div>
   );
 
   return <FilterList items={serviceItems} renderItem={serviceCard} />;
