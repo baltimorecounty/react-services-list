d5e53898761e7868d4f4564484496c065d216d70
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-01-14 15:45:39 -0500

    Added styles to handle popularity star

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -8,29 +8,36 @@ import React from "react";
 
 let starImg = {
   fontSize: ".2em",
-  float: "right",
-  marginTop: "0px",
-  width: "10px"
+  top: "0px",
+  right: "0px",
+  position: "absolute",
+  color: "gray"
+};
+
+let container = {
+  position: "relative"
 };
 
 const ServiceCard = ({ name, department, url, icon, rank }) => (
   <Card>
     <a href={url}>
       <CardContent>
-        <div className="dg_icon-container">
-          <i className={icon} aria-hidden="true"></i>
-          {rank > 0 ? (
-            <span className="sr-only">
-              {`${name} is one of baltimore county's most popular services`}
-            </span>
-          ) : null}
-          <i
-            className={rank > 0 ? "fas fa-star" : ""}
-            style={starImg}
-            aria-hidden="true"
-          />
+        <div style={container}>
+          <div className="dg_icon-container">
+            <i className={icon} aria-hidden="true"></i>
+            {rank > 0 ? (
+              <span className="sr-only">
+                {`${name} is one of baltimore county's most popular services`}
+              </span>
+            ) : null}
+            <i
+              className={rank > 0 ? "fas fa-star" : ""}
+              style={starImg}
+              aria-hidden="true"
+            />
+          </div>
+          <h3>{name}</h3>
         </div>
-        <h3>{name}</h3>
       </CardContent>
       <CardFooter>
         <p>{department}</p>
