8fbc26faf0c251f870780e37e699376e1178f624
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-01-15 11:14:01 -0500

    Added legend for popular star

--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -6,18 +6,35 @@ import useServices from "../hooks/useServices";
 const ServiceList = () => {
   const { serviceItems = [], isLoading } = useServices();
 
+  let legendStar = {
+    fontSize: "20px",
+    color: "gray",
+    "text-align": "left",
+    width: "100%"
+  };
+
   return isLoading ? (
     <p>Loading Baltimore County services...</p>
   ) : (
-    <div className="row">
-      <FilterList
-        items={serviceItems}
-        renderItem={props => (
-          <div className="d-flex col-lg-3 col-md-6 col-sm-6">
-            <ServiceCard {...props} />
-          </div>
-        )}
-      />
+    <div>
+      <div>
+        <i className="fas fa-star" aria-hidden="true" style={legendStar}>
+          - Indicates a top 20 popular service
+        </i>
+        <span className="sr-only">
+          {`The star indicates a top 20 popular service`}
+        </span>
+      </div>
+      <div className="row">
+        <FilterList
+          items={serviceItems}
+          renderItem={props => (
+            <div className="d-flex col-lg-3 col-md-6 col-sm-6">
+              <ServiceCard {...props} />
+            </div>
+          )}
+        />
+      </div>
     </div>
   );
 };
