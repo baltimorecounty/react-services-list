df7efee38cf6442a00b279aded1dcab1714dfa00
martypowell <jpowel25@gmail.com>
2020-01-16 12:31:16 -0500

    added prop types

--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
 
 const FilterList = ({
   as: As = React.Fragment,
@@ -9,6 +9,10 @@ const FilterList = ({
 }) => <As {...props}>{items.map(renderItem)}</As>;
 
 FilterList.propTypes = {
+  /** render the components container as this type of thing, can be another component or a string if it's a basic html element */
+  as: PropTypes.any,
+  /** list of data items to display */
+  items: PropTypes.array,
   /** function that renders a component passed through*/
   renderItem: PropTypes.func.isRequired
 };
