8cc15768309ef1e9d067f27ca71a2ebea786214a
martypowell <jpowel25@gmail.com>
2020-02-19 10:55:37 -0500

    implemented filter based on recommendation

--- src/common/Filter.js
+++ src/common/Filter.js
@@ -7,12 +7,21 @@ const FilterServices = (
     .filter(item => !shouldShowMostPopularServices || item.rank > 0)
     .filter(item => !searchText.trim || filterByTextInput(item, searchText));
 
-const filterByTextInput = (item, searchText) => {
-  return (
-    item.name.toLowerCase().indexOf(searchText.trim().toLowerCase()) !== -1 ||
-    item.department.toLowerCase().indexOf(searchText.trim().toLowerCase()) !==
-      -1
+const generateWordMatchRegex = word => "(?=.*" + word + ")";
+
+const filterByTextInput = ({ name, department }, searchText) => {
+  const searchWordsRegex = searchText
+    .split(" ")
+    .map(field => generateWordMatchRegex(field))
+    .join("");
+  const fieldsAsString = [name, department].join(" ").toLowerCase();
+  const isTextMatch =
+    fieldsAsString.indexOf(searchText.toLowerCase().trim()) > -1;
+  const isSearchWordsMatch = fieldsAsString.match(
+    new RegExp(searchWordsRegex + "+", "gi")
   );
+
+  return isTextMatch || isSearchWordsMatch;
 };
 
 export { FilterServices };
