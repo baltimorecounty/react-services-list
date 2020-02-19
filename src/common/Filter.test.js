f9679b9defb5aafe7c0269701d9f01019c6074a8
martypowell <jpowel25@gmail.com>
2020-02-19 09:45:04 -0500

    added todo test for full text search

---
+++ src/common/Filter.test.js
@@ -0,0 +1,86 @@
+import { FilterService } from "./Filter";
+
+const mockServices = [
+  {
+    name: "Adopt a Pet",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-heart",
+    rank: 5,
+    department: "Information Technology"
+  },
+  {
+    name: "Apply for a Job",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-skiing",
+    rank: 2,
+    department: "Information Technology"
+  },
+  {
+    name: "Citations",
+    url:
+      "https://beta.baltimorecountymd.gov/departments/budfin/citations/index.html",
+    icon: "fas fa-receipt",
+    rank: 3,
+    department: "Law Office"
+  },
+  {
+    name: "County Code",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-fighter-jet",
+    rank: 4,
+    department: "Information Technology"
+  },
+  {
+    name: "Find Your Zoning",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-heart",
+    rank: 5,
+    department: "Information Technology"
+  },
+  {
+    name: "Jury Duty",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-heart",
+    rank: 6,
+    department: "Law Office"
+  },
+  {
+    name: "Police News",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-starship-freighter",
+    rank: 7,
+    department: "Information Technology"
+  },
+  {
+    name: "Quality Assurance",
+    url: "https://beta.baltimorecountymd.gov/services.html",
+    icon: "fas fa-drafting-compass",
+    rank: 0,
+    department: "Information Technology"
+  },
+  {
+    name: "Rec and Parks",
+    url: "http://staging.baltimorecountymd.gov/recreation/index.html",
+    icon: "fas fa-tree",
+    rank: 8,
+    department: "Information Technology"
+  },
+  {
+    name: "Trash and Recycling",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-trash",
+    rank: 9,
+    department: "Information Technology"
+  },
+  {
+    name: "View Daily Docket",
+    url: "https://beta.baltimorecountymd.gov/prototyping/adoption.html",
+    icon: "fas fa-heart",
+    rank: 10,
+    department: "Information Technology"
+  }
+];
+
+test.todo("should retrieve full card contents", () => {
+  const actual = FilterService(mockServices, null, "citations law office");
+});
