1de529418c47cad584239a76a1d833ea4864eaaa
martypowell <jpowel25@gmail.com>
2020-05-24 14:46:50 -0400

    updated styeles

--- src/components/ListLegend.jsx
+++ src/components/ListLegend.jsx
@@ -1,14 +1,14 @@
-import React from "react";
 import { MostPopularServiceIconStyles } from "../styles";
+import React from "react";
 
 const ListLegend = ({ icon, text }) => (
   <div className="dg_legend_container">
     <i
-      className={icon}
+      className={icon + " dg_popular_star"}
       aria-hidden="true"
       style={MostPopularServiceIconStyles}
     ></i>
-    <p className="dg_legend_text">{text}</p>
+    <p style={{ margin: 0 }}>{text}</p>
   </div>
 );
 
