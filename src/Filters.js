06b61a00770c088d3b8d09c9f4cdaddee32a94c8
martypowell <jpowel25@gmail.com>
2020-05-25 20:45:19 -0400

    removed unwanted filter

--- src/Filters.js
+++ src/Filters.js
@@ -4,17 +4,6 @@ const Filters = [
     displayName: "Filter",
     options: [{ label: "Show popular services", value: "true" }],
   },
-  {
-    targetApiField: "tags",
-    displayName: "I Want To",
-    options: [
-      { label: "Apply", value: "apply" },
-      { label: "Contact", value: "contact" },
-      { label: "Follow Up", value: "follow-up" },
-      { label: "Pay", value: "pay" },
-      { label: "Report", value: "report" },
-    ],
-  },
 ];
 
 export default Filters;
