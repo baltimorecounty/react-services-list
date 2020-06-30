6769fc6b2e38f1299de1e932d2d325cb60654dc4
martypowell <jpowel25@gmail.com>
2020-06-30 09:57:35 -0400

    removed unused files

--- src/components/ListCounter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListCounter = ({ count, total }) => (
-  <p className="dg_service_counter">{`Showing ${count} of ${total} services`}</p>
-);
-
-export default ListCounter;
