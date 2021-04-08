0b54b134cf49ae84e3c60f757506f613f674db7a
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-04-08 08:06:35 -0400

    wording update

--- src/common/Message.js
+++ src/common/Message.js
@@ -15,22 +15,23 @@ const NoResultsMessage = () => {
     <div class="dg_alert status warning" style={Styles}>
       <span class="dg_alert__status">Services Help</span>
       <p>
-        It looks like the Service you're looking for isn't currently listed.
-        Please try one of the following options:
+        It looks like the Service you're looking for isn't currently listed. Try
+        one of the following options:
       </p>
       <ul>
         <li>
-          Search for a related page on our website{" "}
+          Search for a{" "}
           <a
             href={`https://www.baltimorecountymd.gov/search/results.html?q=${filter}`}
           >
-            here
+            related page
           </a>
         </li>
         <li>
-          Report a problem or request a service by dialing 311 or use the online
-          reporter{" "}
-          <a href="https://www.baltimorecountymd.gov/report-problem#/">tool</a>
+          Report a problem or request a service by dialing 311 or use the{" "}
+          <a href="https://www.baltimorecountymd.gov/report-problem#/">
+            online reporter tool
+          </a>
         </li>
         <li>
           Contact a Baltimore County{" "}
